ef3ce597f0571fb4f67afda314288f5b9f68c3ab
10900036canadainc <10900036canadainc@gmail.com>
2022-02-04 23:00:10 -0700

    initial commit

---
+++ deploy-simple-service-to-kubernetes/create-service.sh
@@ -0,0 +1,15 @@
+cat << EOF | kubectl apply -f -
+kind: Service
+apiVersion: v1
+metadata:
+  name: store-products
+spec:
+  selector:
+    app: store-products
+  ports:
+  - protocol: TCP
+    port: 80
+    targetPort: 80
+EOF
+
+#kubectl exec busybox -- curl -s store-products
